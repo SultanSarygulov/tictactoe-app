6044238c9614d418425cbbd857edc97c92afb91c
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-11-21 00:15:45 +0600

    Redesigned project

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.example.tictactoe
 
-import android.graphics.Color.red
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.TextView
 import com.example.tictactoe.databinding.ActivityMainBinding
-import java.util.logging.Logger.global
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var turn = 0
+    val redSquare = R.drawable.soft_square_red
+    val blueSquare = R.drawable.soft_square_blue
+    val blackSquare = R.drawable.soft_square_black
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setListeners()
+
+        binding.againButton.setOnClickListener {
+            startAgain()
+        }
+
     }
+
+
 
     private fun setListeners(){
 
@@ -33,33 +45,95 @@ class MainActivity : AppCompatActivity() {
             binding.tile33)
 
         for (item in clickableViews){
+            Log.d("TicTac", "${item.id}")
 
-            item.setOnClickListener{ makeColored(it) }
+            item.setOnClickListener{ makeColored(it as TextView) }
 
         }
     }
 
-    var turn = 0
-    private fun makeColored(view: View){
+    private fun makeColored(view: TextView){
 
-        val tileColor = if (turn % 2 == 0){
-            R.color.myRed
+        var currentSquare = blackSquare
+        var sign = ""
+        if (turn % 2 == 0){
+            currentSquare = redSquare
+            sign = "X"
+            binding.player1.setBackgroundResource(blackSquare)
+            binding.player2.setBackgroundResource(blueSquare)
         } else {
-            R.color.myBlue
+            currentSquare = blueSquare
+            sign = "O"
+            binding.player1.setBackgroundResource(redSquare)
+            binding.player2.setBackgroundResource(blackSquare)
+
+        }
+        if (view.text == ""){
+
+            when (view.id) {
+                R.id.tile_1_1 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_2_1 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_3_1 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_1_2 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_2_2 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_3_2 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_1_3 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_2_3 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+                R.id.tile_3_3 -> {
+                    view.setBackgroundResource(currentSquare)
+                    view.text = sign
+                }
+            }
+            turn++
         }
 
-        when(view.id){
-            R.id.tile_1_1 -> view.setBackgroundResource(tileColor)
-            R.id.tile_2_1 -> view.setBackgroundResource(tileColor)
-            R.id.tile_3_1 -> view.setBackgroundResource(tileColor)
-            R.id.tile_1_2 -> view.setBackgroundResource(tileColor)
-            R.id.tile_2_2 -> view.setBackgroundResource(tileColor)
-            R.id.tile_3_2 -> view.setBackgroundResource(tileColor)
-            R.id.tile_1_3 -> view.setBackgroundResource(tileColor)
-            R.id.tile_2_3 -> view.setBackgroundResource(tileColor)
-            R.id.tile_3_3 -> view.setBackgroundResource(tileColor)
-        }
 
-        turn++
+
     }
+
+    private fun startAgain() {
+        val clickableViews = listOf(
+            binding.tile11,
+            binding.tile21,
+            binding.tile31,
+            binding.tile12,
+            binding.tile22,
+            binding.tile32,
+            binding.tile13,
+            binding.tile23,
+            binding.tile33)
+
+        clickableViews.forEach{
+            it.text = ""
+            it.setBackgroundResource(blackSquare)
+        }
+        binding.player1.setBackgroundResource(redSquare)
+        binding.player2.setBackgroundResource(blackSquare)
+        turn = 0
+    }
+
 }
